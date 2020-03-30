2f5fb4d462ad87579a7aee075844eef13cf01273
liao20081228 <liao20081228@126.com>
2020-03-30 23:20:03 +0800

    2020年 03月 30日 星期一 23:20:03 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- change_blog_pic_addr.sh
+++ change_blog_pic_addr.sh
@@ -4,10 +4,13 @@ for e in `ls`
 do
 	if [ -d $e ]
 	then
-		sed -i "s/raw.githubusercontent.com\/liao20081228\/blog\/master\/图片/www.gitee.com\/liao20081228\/blog_pictures\/raw\/master/g" rdma
+		for ee in `ls $e`
+		do
+			sed -i "s/raw.githubusercontent.com\/liao20081228\/blog\/master\/图片/gitee.com\/liao20081228\/blog_pictures\/raw\/master/g" $ee
+			sed -i "s/www.github.com\/liao20081228\/blog\/raw\/master\/图片/gitee.com\/liao20081228\/blog_pictures\/raw\/master" $ee
+		done
 	else
-
+		sed -i "s/raw.githubusercontent.com\/liao20081228\/blog\/master\/图片/gitee.com\/liao20081228\/blog_pictures\/raw\/master/g" $e
+		sed -i "s/www.github.com\/liao20081228\/blog\/raw\/master\/图片/gitee.com\/liao20081228\/blog_pictures\/raw\/master" $e
 	fi
-
 done
-	`
